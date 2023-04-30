3b83ae2b3f64402e34fbd688dfcab3142dfa78ea
KC_Dev <burakkcelebi@hotmail.com>
2023-04-30 20:37:55 +0100

    Built! JS logic code is done
    JS logic is to check if the pw inputs match if not prevent the from from being submitted and change the colour of the inputs as well as making the pw warning massage visible

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// TODO:
+"use strict";
+
+const pw1 = document.querySelector("#pwd-1");
+const pw2 = document.querySelector("#pwd-2");
+const form = document.querySelector("form");
+const pwWarn = document.querySelector(".pwWarning");
+
+form.addEventListener("submit", (e) => {
+  console.log(pw1.value);
+  console.log(pw2.value);
+
+  if (pw1.value !== pw2.value) {
+    e.preventDefault();
+    pwWarn.style.visibility = "visible";
+  }
+});
